4bec0bf39210f6ed7ffa28b5f7307d03a8562130
YuudaiHiiro <jamroack@gmail.com>
2023-09-21 20:02:54 +0300

    updated util.py and period.py

--- logic/period.py
+++ logic/period.py
@@ -12,13 +12,14 @@ def per():
 
     # Set up the options for months and years for the dropdown.
     current_year = datetime.today().year
+    current_month_index = datetime.today().month - 1  # -1 because lists are 0-indexed
     years = [current_year + i for i in range(8)]
     months = list(calendar.month_name[1:])
 
     # Display the header for data entry and dropdowns for selecting the month and year.
     st.header(f"Data Entry in: {currency}")
     col1, col2 = st.columns(2)
-    month = col1.selectbox("Select Month", months)
+    month = col1.selectbox("Select Month", months, index=current_month_index)
     year = col2.selectbox("Select Year", years)
 
     return month, year
