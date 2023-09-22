313be5bb8282f31709ef02d1aae829ced91e0ab8
YuudaiHiiro <jamroack@gmail.com>
2023-09-22 15:24:49 +0300

    changes from csv to json

--- logic/currency.py
+++
@@ -1,7 +0,0 @@
-# currency.py
-
-def curr():
-
-    # Define the currency to be used throughout the application.
-    currency = "RON"
-    return currency
--- logic/util.py
+++
@@ -1,6 +0,0 @@
-# util.py
-
-import os
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_NAME = os.path.join(SCRIPT_DIR, 'data.csv')
